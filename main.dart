1fcaa7f74e53cd2e0b4955bc856081c939a3cb94
sofiastacanelli <104329228+sofiastacanelli@users.noreply.github.com>
2023-06-29 12:21:49 -0300

    Avaliação

--- main.dart
+++ main.dart
@@ -1,73 +1,155 @@
 import 'package:flutter/material.dart';
-import './questionario.dart';
-import './questao.dart';
-import './resposta.dart';
 
-main() {
-  runApp(AulaComponentes());
+void main() {
+  runApp(QuestionnaireApp());
 }
 
-class AulaComponentes extends StatefulWidget{
+class QuestionnaireApp extends StatelessWidget {
   @override
-  State<AulaComponentes> createState() => _AulaComponentesState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Questionnaire App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
 }
 
-class _AulaComponentesState extends State<AulaComponentes> {
-
-  var perguntaAtual = 0;
-  var cor = Colors.white;
-
-  final List<Map<String, Object>> questionario = [
-    {
-      "pergunta": "Qual a sua cor favorita?",
-      "respostas": ["Amarelo", "Preto", "Branco", "Azul", "Vermelho"]
-    },
-    {
-      "pergunta": "Qual é seu animal favorito?",
-      "respostas": ["Cachorro", "Gato", "Tartaruga", "Periquito"]
-    },
-    {
-      "pergunta": "Qual sua linguagem favorita?",
-      "respostas": ["Python", "Java", "JavaScript"]
-    },
-  ];
-
-  bool get temPergunta {
-    return perguntaAtual < questionario.length;
-  }
-  
-  void acao() {
-    setState(() {
-      perguntaAtual++;
-    });
-    print(perguntaAtual);
-  }
-
+class HomePage extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-
-    List<Widget> respostas = [];
-
-    if (temPergunta) {
-      for (var resposta in questionario[perguntaAtual]["respostas"] as List<String>) {
-        respostas.add(
-          Resposta(resposta, acao),
-        );
-      }
-    }
-    
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: temPergunta ? Questao(questionario[perguntaAtual]["pergunta"].toString()) : Questao("Terminou"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Questionário'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sistema de Perguntas e Respostas',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 24.0),
+            ElevatedButton(
+              child: Text('Iniciar Questionário'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => QuestionnairePage()),
+                );
+              },
+            ),
+          ],
         ),
-        body: temPergunta
-           ? Questionario(
-                perguntas: questionario,
-                perguntaAtual: perguntaAtual,
-                onRespostaSelecionada: () => acao(),
-              )
-         : Text("Resultado"),
       ),
     );
   }
+}
+
+class QuestionnairePage extends StatefulWidget {
+  @override
+  _QuestionnairePageState createState() => _QuestionnairePageState();
+}
+
+class _QuestionnairePageState extends State<QuestionnairePage> {
+  final List<Question> questions = [
+    Question('Qual é o seu nome?'),
+    Question('Qual é a sua idade?'),
+    Question('Qual é a sua cidade?'),
+  ];
+
+  final Map<Question, String> userAnswers = {};
+
+  int currentQuestionIndex = 0;
+  final TextEditingController answerController = TextEditingController();
+
+  void submitAnswer() {
+    String answer = answerController.text.trim();
+    userAnswers[questions[currentQuestionIndex]] = answer;
+    answerController.clear();
+    if (currentQuestionIndex < questions.length - 1) {
+      setState(() {
+        currentQuestionIndex++;
+      });
+    } else {
+      // Exibe as respostas do usuário
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Respostas do Usuário'),
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                for (var entry in userAnswers.entries)
+                  Text('${entry.key.question}: ${entry.value}'),
+              ],
+            ),
+            actions: [
+              ElevatedButton(
+                child: Text('Fechar'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    answerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Questionário'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              questions[currentQuestionIndex].question,
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              controller: answerController,
+              onChanged: (value) {
+                // Não é necessário chamar o submitAnswer aqui, pois vamos usar um botão para enviar a resposta
+              },
+            ),
+            ElevatedButton(
+              child: Text('Enviar Resposta'),
+              onPressed: () {
+                submitAnswer();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Question {
+  final String question;
+
+  Question(this.question);
 }
